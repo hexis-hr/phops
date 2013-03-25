6bbcb02911f35bcf7d008699a0dc63a00454c37d
luka8088 <luka8088@owave.net>
2013-03-25 09:11:18 +0100

    added version_design
    Signed-off-by: luka8088 <luka8088@owave.net>

--- environment.php
+++ environment.php
@@ -62,4 +62,5 @@ if (!isset($_SERVER['unitTest_wdUrl']) || !$_SERVER['unitTest_wdUrl'])
 define("version_assert", $_SERVER['environment'] != 'production');
 define("version_unittest", $_SERVER['environment'] == 'unittest');
 define("version_development", $_SERVER['environment'] == 'development');
+define("version_design", $_SERVER['environment'] == 'design');
 
