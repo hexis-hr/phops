ff59f6a4586c45b4bd3460c520a4f8f9d755ccd7
luka8088 <luka8088@owave.net>
2013-07-17 09:34:06 +0200

    added codeBaseChanged
    Signed-off-by: luka8088 <luka8088@owave.net>

--- index.php
+++ index.php
@@ -27,6 +27,23 @@ function codeBaseTimestamp () {
   return $timestamp;
 }
 
+function codeBaseChanged () {
+  static $includedFiles = array();
+  static $result = true;
+  $newIncludedFiles = get_included_files();
+  if (count($includedFiles) != count($newIncludedFiles)) {
+    $fingerprintFile = $_SERVER['cachePath'] . '/codeBase_' . sha1(serialize(get_included_files())) . '.timestamp';
+    if (!is_file($fingerprintFile)) {
+      directory(dirname($fingerprintFile));
+      $touchResult = touch($fingerprintFile);
+      enforce($touchResult, "Could not touch '$fingerprintFile'");
+    }
+    $result = codeBaseTimestamp() > filemtime($fingerprintFile);
+    $includedFiles = $newIncludedFiles;
+  }
+  return $result;
+}
+
 // back-compatibility function
 // staticIndex($file, $line, [$key,] $callback)
 function staticIndex ($file, $line, $key, $callback = null) {
